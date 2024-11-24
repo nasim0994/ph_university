c36e09a1f672cbb83b7f5e3a60dc9e4255418a71
nasim0994 <nasimuddin0994@gmail.com>
2024-11-24 12:38:30 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,23 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { carRoute } from './modules/car/carRoute';
 import { orderRoute } from './modules/order/orderRoute';
+import config from './config';
 const app: Application = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req: Request, res: Response) => {
+  res.send(`server is running on port ${config.PORT} 🏃‍♂️‍➡️`);
+});
+
 // use Routes
 app.use('/api/cars', carRoute);
 app.use('/api/orders', orderRoute);
 
 // all undefined routes
-app.all('*', (req, res) => {
+app.all('*', (req: Request, res: Response) => {
   res.status(404).json({
     success: false,
     message: 'Api not found!',
